666d6bf3251bceab67a186e5f6098b69c69b68c6
Maks <37328113+maksbrzezinski@users.noreply.github.com>
2022-12-06 00:46:42 +0000

    Update downloadXkcd.py

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -5,44 +5,44 @@
 import requests, os, bs4
 from html.parser import HTMLParser
 
-url = 'http://xkcd.com'             #Początkowy adres URL.
-os.makedirs('xkcd', exist_ok=True)  #Komiksy są przechowywane w katalogu ./xkcd.
+url = 'http://xkcd.com'             #Initial URL address
+os.makedirs('xkcd', exist_ok=True)  #Comic arts are kept in the catalog ./xkcd.
 while not url.endswith('#'):
     
-    #Pobranie strony.
+    #Gets page
     
-    print('Pobieranie strony %s...' % url)
+    print('Page download %s...' % url)
     parser = HTMLParser
     res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text)
 
-    #Ustalenie adresu URL pliku obrazu komiksu.
+    #Gets URL of a comic file
 
     comicElem = soup.select('#comic img')
     if comicElem == []:
-        print('Nie udało się odnaleźć pliku obrazu komiksu.')
+        print('Fail to find comic image address.')
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
 
     #Pobranie obrazu.
 
-    print('Pobieranie obrazu %s...' % (comicUrl))
+    print('Image download %s...' % (comicUrl))
     res = requests.get(comicUrl)
     res.raise_for_status()
 
-    #Zapis obrazu w katalogu ./xkcd.
+    #Save an image in the ./xkcd catalog.
 
     imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
 
-    #Pobranie adresu URL w przycisku Prev.
+    #Get the 'Prev' button URL.
 
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
     
                            
 
-print('Gotowe!')
+print('Ready!')
